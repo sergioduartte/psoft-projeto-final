f81e9b4f47b71de4aac9869887a5fcb2c85a37e4
arthurcavalcant <arthur.ferreira@ccc.ufcg.edu.br>
2021-04-30 12:36:07 -0300

    Add inicial project structure

---
+++ src/test/java/com/example/projeto_final_psoft/ProjetoFinalPsoftApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.projeto_final_psoft;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetoFinalPsoftApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
