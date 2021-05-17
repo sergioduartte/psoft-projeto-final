34684af46cd4817eeadb444b21aba73a58a9f196
sergioduartte <sduartecc@gmail.com>
2021-05-17 06:00:08 -0300

    us10 ok

--- src/main/java/com/ufcg/psoft/projeto_final/services/cidadao/CidadaoService.java
+++ src/main/java/com/ufcg/psoft/projeto_final/services/cidadao/CidadaoService.java
@@ -2,6 +2,7 @@ package com.ufcg.psoft.projeto_final.services.cidadao;
 
 import com.ufcg.psoft.projeto_final.DTOs.AtualizaCidadaoDTO;
 import com.ufcg.psoft.projeto_final.DTOs.CidadaoDTO;
+import com.ufcg.psoft.projeto_final.exceptions.CadastroCidadaoException;
 import com.ufcg.psoft.projeto_final.models.*;
 import com.ufcg.psoft.projeto_final.errors.CidadaoCadastroInvalido;
 import com.ufcg.psoft.projeto_final.errors.LoginTipoInvalido;
@@ -15,6 +16,6 @@ public interface CidadaoService {
 
 	Cidadao saveCidadao(CidadaoDTO cidadaoDTO) throws LoginTipoInvalido, CidadaoCadastroInvalido;
 	Cidadao getCidadao(Long cpf) throws CidadaoNaoEncontradoException;
-	Cidadao atualizaCadastro(Long cpf, AtualizaCidadaoDTO atualizaCidadaoDTO) throws CidadaoNaoEncontradoException;
+	Cidadao atualizaCadastro(AtualizaCidadaoDTO atualizaCidadaoDTO) throws CidadaoNaoEncontradoException, CadastroCidadaoException;
 
 }
