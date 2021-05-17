4187e63cb7533c186f0a2b8c8ecbc505ccbb429f
iele passos <iele.passos@ccc.ufcg.edu.br>
2021-05-17 09:02:45 -0300

    Documentacao dos controllers

--- src/main/java/com/ufcg/psoft/projeto_final/controllers/LoginController.java
+++ src/main/java/com/ufcg/psoft/projeto_final/controllers/LoginController.java
@@ -5,6 +5,9 @@ import com.ufcg.psoft.projeto_final.errors.LoginInvalido;
 import com.ufcg.psoft.projeto_final.errors.LoginTipoInvalido;
 import com.ufcg.psoft.projeto_final.services.login.LoginResponse;
 import com.ufcg.psoft.projeto_final.web_security.JwtTokenService;
+
+import io.swagger.annotations.ApiOperation;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -18,6 +21,7 @@ public class LoginController {
     @Autowired
     private JwtTokenService JWTService;
 
+    @ApiOperation(value = "Realiza login no sistema")
     @PostMapping("/auth/login")
     public ResponseEntity<?> login(@RequestBody LoginDTO loginDTO) throws LoginInvalido, LoginTipoInvalido {
         LoginResponse jwt = JWTService.autentica(loginDTO);
--- src/main/java/com/ufcg/psoft/projeto_final/controllers/VacinaController.java
+++ src/main/java/com/ufcg/psoft/projeto_final/controllers/VacinaController.java
@@ -6,6 +6,9 @@ import com.ufcg.psoft.projeto_final.errors.VacinaCadastroInvalido;
 import com.ufcg.psoft.projeto_final.errors.VacinaNaoEncontrada;
 import com.ufcg.psoft.projeto_final.models.Vacina;
 import com.ufcg.psoft.projeto_final.services.vacina.VacinaService;
+
+import io.swagger.annotations.ApiOperation;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -20,18 +23,19 @@ public class VacinaController {
     @Autowired
     VacinaService vacinaService;
 
+    @ApiOperation(value = "Retorna todas as vacinas cadastradas no sistema")
     @GetMapping("/vacina")
     public ResponseEntity<List<Vacina>> getVacinas() {
         List<Vacina> vacinas = vacinaService.getVacina();
         return new ResponseEntity<List<Vacina>>(vacinas, HttpStatus.OK);
     }
-
+    @ApiOperation(value = "Retorna uma vacina a partir do seu Id")
     @GetMapping("/vacina/{id}")
     public ResponseEntity<Vacina> getVacinaById(@RequestParam Long id) throws VacinaNaoEncontrada {
         Vacina vacina = vacinaService.getVacinaById(id);
         return new ResponseEntity<Vacina>(vacina, HttpStatus.OK);
     }
-
+    @ApiOperation(value = "Cadastra uma vacina no sistema")
     @PostMapping("/vacina")
     public ResponseEntity<Vacina> vacina(@RequestBody VacinaDTO vacinaDTO) throws VacinaCadastroInvalido {
         Vacina novaVacina = vacinaService.saveVacina(vacinaDTO);
